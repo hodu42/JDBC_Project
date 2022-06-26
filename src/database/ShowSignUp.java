75552fa9ceb5840e4ef63071e615983835cc1caf
DoDoGaMaRu <nm09090@gmail.com>
2022-06-26 14:30:42 +0900

    update method

--- src/database/ShowSignUp.java
+++ src/database/ShowSignUp.java
@@ -5,18 +5,21 @@ import java.sql.*;
 public class ShowSignUp {
     protected String show(Connection con, int stdId) {
         StringBuilder sb = new StringBuilder();
-        String query = ("");
+        String query = ("SELECT subject.subjectid, signup.scid, subject.subjectname FROM SignUp, subject"
+                + "WHERE signup.id = " + "stdId"
+                + " AND signup.subjectid = subject.subjectid;");
 
         try {
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery(query);
 
+            sb.append("subjectId\t-scId\tsubjectName\n");
             while(rs.next()) {
                 String subjectId = rs.getString("subjectId");
                 int scId = rs.getInt("scId");
                 String subjectName = rs.getString("subjectName");
 
-                sb.append(subjectId + "-" + scId + " " + subjectName);
+                sb.append(subjectId + "\t-" + scId + "\t" + subjectName);
                 sb.append("\n\n");
             }
             rs.close();
